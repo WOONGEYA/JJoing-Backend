28e6dbd1128a799d38755312b5aa56f1e8791715
Woongbin06 <kob05314@gmail.com>
2023-04-20 17:18:20 +0900

    프로젝트 생성

---
+++ src/main/java/com/woongeya/zoing/ZoingApplication.java
@@ -0,0 +1,13 @@
+package com.woongeya.zoing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZoingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZoingApplication.class, args);
+	}
+
+}
