d0a3d2cc78f79571705748e31ab5186d5d5b8a2e
Woongbin06 <kob05314@gmail.com>
2023-05-18 23:44:59 +0900

    FIX :: Security Context에 유저 정보가 저장되지 않는 버그 수정

--- src/main/java/com/woongeya/zoing/global/jwt/auth/JwtAuth.java
+++ src/main/java/com/woongeya/zoing/global/jwt/auth/JwtAuth.java
@@ -8,7 +8,6 @@ import io.jsonwebtoken.Claims;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.parameters.P;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 
