b0a2aa05206a5f8e54180a64eb04b53fa75b7edf
Woongbin06 <kob05314@gmail.com>
2023-06-15 23:21:51 +0900

    DELETE :: 깃허브 로그인 삭제

--- src/main/java/com/woongeya/zoing/domain/auth/presetation/AuthController.java
+++ src/main/java/com/woongeya/zoing/domain/auth/presetation/AuthController.java
@@ -1,6 +1,5 @@
 package com.woongeya.zoing.domain.auth.presetation;
 
-import com.woongeya.zoing.domain.auth.service.OAuth2GithubService;
 import com.woongeya.zoing.domain.auth.service.OAuth2GoogleService;
 import com.woongeya.zoing.global.jwt.dto.TokenResponseDto;
 import lombok.RequiredArgsConstructor;
@@ -13,15 +12,9 @@ import org.springframework.web.bind.annotation.*;
 public class AuthController {
 
     private final OAuth2GoogleService googleService;
-    private final OAuth2GithubService githubService;
 
     @GetMapping("/code/google")
     public TokenResponseDto loginOfGoogle(@Validated @RequestParam(name = "code") String code) {
         return googleService.getJwtToken(code);
-    }
-
-    @GetMapping("/code/github")
-    public TokenResponseDto loginOfGithub(@Validated @RequestParam(name = "code") String code) {
-        return githubService.getJwtToken(code);
     }
 }
