cab10ce21d64ef942e0fb8f08c7f6e0a45569f10
Woongbin06 <kob05314@gmail.com>
2023-07-03 16:36:13 +0900

    FEAT :: 사용자의 팔로워 조회

--- src/main/java/com/woongeya/zoing/domain/follow/presetation/FollowController.java
+++ src/main/java/com/woongeya/zoing/domain/follow/presetation/FollowController.java
@@ -2,6 +2,7 @@ package com.woongeya.zoing.domain.follow.presetation;
 
 import com.woongeya.zoing.domain.follow.presetation.dto.reponse.FollowResponseDto;
 import com.woongeya.zoing.domain.follow.service.CreateFollowService;
+import com.woongeya.zoing.domain.follow.service.FindFollowerService;
 import com.woongeya.zoing.domain.follow.service.FindFollowingService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
@@ -15,6 +16,7 @@ public class FollowController {
 
     private final CreateFollowService createFollowService;
     private final FindFollowingService findFollowingService;
+    private final FindFollowerService findFollowerService;
 
     @PostMapping("/{id}")
     public void createFollow(@PathVariable Long id) {
@@ -24,5 +26,10 @@ public class FollowController {
     @GetMapping("/{id}/following")
     public List<FollowResponseDto> findFollwing(@PathVariable Long id) {
         return findFollowingService.execute(id);
+    }
+
+    @GetMapping("/{id}/follower")
+    public List<FollowResponseDto> findFollower(@PathVariable Long id) {
+        return findFollowerService.execute(id);
     }
 }
