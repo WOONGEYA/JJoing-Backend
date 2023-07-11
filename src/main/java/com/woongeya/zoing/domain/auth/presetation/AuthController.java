b905b6577cfd26c3b8ade2cff4d341dd5b2f60fa
Woongbin06 <kob05314@gmail.com>
2023-07-12 00:16:01 +0900

    FEAT :: 구글 로그인 URL을 반환

--- src/main/java/com/woongeya/zoing/domain/auth/presetation/AuthController.java
+++ src/main/java/com/woongeya/zoing/domain/auth/presetation/AuthController.java
@@ -1,5 +1,6 @@
 package com.woongeya.zoing.domain.auth.presetation;
 
+import com.woongeya.zoing.domain.auth.service.OAuth2GoogleLinkService;
 import com.woongeya.zoing.domain.auth.service.OAuth2GoogleService;
 import com.woongeya.zoing.global.jwt.dto.TokenResponseDto;
 import lombok.RequiredArgsConstructor;
@@ -7,13 +8,19 @@ import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/login/oauth2")
+@RequestMapping("/login")
 @RequiredArgsConstructor
 public class AuthController {
 
     private final OAuth2GoogleService googleService;
+    private final OAuth2GoogleLinkService oAuth2GoogleLinkService;
 
-    @GetMapping("/code/google")
+    @GetMapping("/google")
+    public String getGoogleAuthLink() {
+        return oAuth2GoogleLinkService.execute();
+    }
+
+    @PostMapping()
     public TokenResponseDto loginOfGoogle(@Validated @RequestParam(name = "code") String code) {
         return googleService.getJwtToken(code);
     }
---
+++ src/main/java/com/woongeya/zoing/domain/auth/service/OAuth2GoogleLinkService.java
@@ -0,0 +1,22 @@
+package com.woongeya.zoing.domain.auth.service;
+
+import com.woongeya.zoing.global.oauth.AuthProperties;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class OAuth2GoogleLinkService {
+
+    private static final String baseUrl = "https://accounts.google.com/o/oauth2/auth?";
+    private final AuthProperties authProperties;
+
+    public String execute() {
+        return String.format(
+                baseUrl +
+                "?client_id=%s&redirect_uri=%s&response_type=code&scope=https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/userinfo.profile",
+                authProperties.getClientId(),
+                authProperties.getRedirectUri()
+        );
+    }
+}
