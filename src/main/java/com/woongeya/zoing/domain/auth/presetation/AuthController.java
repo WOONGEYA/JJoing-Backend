c61394f65643603285e0cf2400f3c97841f1b4a7
Woongbin06 <kob05314@gmail.com>
2023-07-13 16:03:37 +0900

    FEAT :: Access 토큰 재발급

--- src/main/java/com/woongeya/zoing/domain/auth/presetation/AuthController.java
+++ src/main/java/com/woongeya/zoing/domain/auth/presetation/AuthController.java
@@ -2,10 +2,13 @@ package com.woongeya.zoing.domain.auth.presetation;
 
 import com.woongeya.zoing.domain.auth.service.OAuth2GoogleLinkService;
 import com.woongeya.zoing.domain.auth.service.OAuth2GoogleService;
+import com.woongeya.zoing.domain.auth.service.RefreshTokenService;
 import com.woongeya.zoing.global.jwt.dto.TokenResponseDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
+
+import javax.validation.constraints.NotBlank;
 
 @RestController
 @RequestMapping("/login")
@@ -14,6 +17,7 @@ public class AuthController {
 
     private final OAuth2GoogleService googleService;
     private final OAuth2GoogleLinkService oAuth2GoogleLinkService;
+    private final RefreshTokenService refreshTokenService;
 
     @GetMapping("/google")
     public String getGoogleAuthLink() {
@@ -23,5 +27,10 @@ public class AuthController {
     @PostMapping("/google")
     public TokenResponseDto loginOfGoogle(@Validated @RequestParam(name = "code") String code) {
         return googleService.execute(code);
+    }
+
+    @PutMapping("")
+    public TokenResponseDto refreshToken(@RequestHeader(value = "Refresh-Token") @NotBlank String refreshToken) {
+        return refreshTokenService.execute(refreshToken);
     }
 }
