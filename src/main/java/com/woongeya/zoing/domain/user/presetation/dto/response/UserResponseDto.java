0c00c20e0786b9ccb83b64a4d10a01e9830def66
Woongbin06 <kob05314@gmail.com>
2023-08-18 14:41:46 +0900

    FEAT :: 로그인된 유저 정보 불러올 때 새로운 컬럼도 같이 불러옴

--- src/main/java/com/woongeya/zoing/domain/user/presetation/dto/response/UserResponseDto.java
+++ src/main/java/com/woongeya/zoing/domain/user/presetation/dto/response/UserResponseDto.java
@@ -10,12 +10,16 @@ public class UserResponseDto {
     private String email;
     private String imgUrl;
     private String school;
+    private String githubUrl;
+    private String statusMessage;
 
     @Builder
-    public UserResponseDto(String name, String email, String imgUrl, String school) {
+    public UserResponseDto(String name, String email, String imgUrl, String school, String githubUrl, String statusMessage) {
         this.name = name;
         this.email = email;
         this.imgUrl = imgUrl;
         this.school = school;
+        this.githubUrl = githubUrl;
+        this.statusMessage = statusMessage;
     }
 }
