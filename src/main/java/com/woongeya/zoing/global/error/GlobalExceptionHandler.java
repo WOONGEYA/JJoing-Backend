5f40a5eae166a0dfbd7b4ff3baba54a39fd18620
Woongbin06 <kob05314@gmail.com>
2023-09-12 19:06:59 +0900

    FEAT :: GlobalExceptionHandler 생성

---
+++ src/main/java/com/woongeya/zoing/global/error/GlobalExceptionHandler.java
@@ -0,0 +1,62 @@
+package com.woongeya.zoing.global.error;
+
+import com.woongeya.zoing.global.error.exception.ZoingException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindException;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import javax.validation.ConstraintViolation;
+import javax.validation.ConstraintViolationException;
+import java.util.HashMap;
+import java.util.Map;
+
+import static com.woongeya.zoing.global.error.exception.ErrorCode.INTERNAL_SERVER_ERROR;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(ZoingException.class)
+    public ResponseEntity<?> handleCustomException(ZoingException e) {
+        return new ResponseEntity<>(new ErrorResponse(e.getErrorCode().getStatus(), e.getErrorCode().getCode(), e.getErrorCode().getMessage()), HttpStatus.valueOf(e.getErrorCode().getStatus()));
+    }
+
+    @ExceptionHandler(BindException.class)
+    @ResponseStatus(value = HttpStatus.BAD_REQUEST)
+    public ResponseEntity<?> bindException(BindException e) {
+        Map<String, String> errorMap = new HashMap<>();
+
+        for (FieldError error : e.getFieldErrors()) {
+            errorMap.put(error.getField(), error.getDefaultMessage());
+        }
+
+        return new ResponseEntity<>(errorMap, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler({ConstraintViolationException.class})
+    @ResponseStatus(value = HttpStatus.BAD_REQUEST)
+    public ResponseEntity<?> handleConstraintViolation(ConstraintViolationException e) {
+        Map<String, String> errorMap = new HashMap<>();
+
+        for (ConstraintViolation<?> violation : e.getConstraintViolations()) {
+            errorMap.put(violation.getPropertyPath().toString(), violation.getMessage());
+        }
+
+        return new ResponseEntity<>(errorMap, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorResponse> handleServerException(Exception ex) {
+        return new ResponseEntity<>(
+                new ErrorResponse(
+                        INTERNAL_SERVER_ERROR.getStatus(),
+                        INTERNAL_SERVER_ERROR.getCode(),
+                        INTERNAL_SERVER_ERROR.getMessage()
+                ),
+                HttpStatus.INTERNAL_SERVER_ERROR
+        );
+    }
+}
