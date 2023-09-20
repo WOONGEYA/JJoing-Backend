f5cb35440f810c31230831d26482c5438f5a7e88
Woongbin06 <kob05314@gmail.com>
2023-09-20 20:14:45 +0900

    FEAT :: 신청 수락 시 수락 알람 생성

--- src/main/java/com/woongeya/zoing/domain/application/service/AcceptApplicationService.java
+++ src/main/java/com/woongeya/zoing/domain/application/service/AcceptApplicationService.java
@@ -2,6 +2,8 @@ package com.woongeya.zoing.domain.application.service;
 
 import com.woongeya.zoing.domain.application.ApplicationFacade;
 import com.woongeya.zoing.domain.application.domain.Application;
+import com.woongeya.zoing.domain.notice.domain.Notification;
+import com.woongeya.zoing.domain.notice.domain.repository.NotificationRepository;
 import com.woongeya.zoing.domain.project.domain.Member;
 import com.woongeya.zoing.domain.project.domain.Project;
 import com.woongeya.zoing.domain.project.domain.repository.CustomMemberRepository;
@@ -24,13 +26,14 @@ public class AcceptApplicationService {
     private final ProjectFacade projectFacade;
     private final CustomMemberRepository customMemberRepository;
     private final MemberRepository memberRepository;
+    private final NotificationRepository notificationRepository;
 
     @Transactional
     public void execute(Long id) {
-        User user = userFacade.getCurrentUser();
+        User writer = userFacade.getCurrentUser();
         Application application = applicationFacade.getApplication(id);
         Project project = projectFacade.getProject(application.getId());
-        Member member = customMemberRepository.findByUserIdAndProjectId(user.getId(), project.getId())
+        Member member = customMemberRepository.findByUserIdAndProjectId(writer.getId(), project.getId())
                 .orElseThrow(() -> MemberNotFoundException.EXCEPTION);
 
         member.acceptApplication(application);
@@ -42,5 +45,13 @@ public class AcceptApplicationService {
                         .build()
         );
         project.increaseCurrentPeople();
+
+        notificationRepository.save(
+                Notification.builder()
+                        .title(writer.getNickName() + " 님이 " + project.getName() + " 프로젝트 쪼잉을 수락하셨어요.")
+                        .content("이제부터 " + project.getName() + " 프로젝트의 일원이 되셨어요!")
+                        .userId(application.getUserId())
+                        .build()
+        );
     }
 }
