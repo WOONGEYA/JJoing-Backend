5e775ec07deaeef42c47ffef2d698ca6fd63b715
Woongbin06 <kob05314@gmail.com>
2023-09-20 20:13:35 +0900

    UPDATE :: 신청 포지션을 Enum -> String

--- src/main/java/com/woongeya/zoing/domain/application/presetation/dto/request/ApplicationCreateRequest.java
+++ src/main/java/com/woongeya/zoing/domain/application/presetation/dto/request/ApplicationCreateRequest.java
@@ -1,10 +1,7 @@
 package com.woongeya.zoing.domain.application.presetation.dto.request;
 
-import com.woongeya.zoing.domain.application.domain.type.ApplicationJobPosition;
 import lombok.Getter;
 
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 
 @Getter
@@ -13,5 +10,5 @@ public class ApplicationCreateRequest {
     private String introduce;
 
     @NotNull
-    private ApplicationJobPosition position;
+    private String position;
 }
