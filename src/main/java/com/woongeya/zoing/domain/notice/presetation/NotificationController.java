0d5a519d8d2c16afe04ced652de891bace795033
Woongbin06 <kob05314@gmail.com>
2023-09-21 17:30:48 +0900

    CHORE :: 알람 조회 기능 컨트롤러 추가

--- src/main/java/com/woongeya/zoing/domain/notice/presetation/NotificationController.java
+++ src/main/java/com/woongeya/zoing/domain/notice/presetation/NotificationController.java
@@ -1,9 +1,24 @@
 package com.woongeya.zoing.domain.notice.presetation;
 
+import com.woongeya.zoing.domain.notice.presetation.dto.response.NotificationResponse;
+import com.woongeya.zoing.domain.notice.service.FindNotificationService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/notification")
 public class NotificationController {
+
+    private final FindNotificationService findNotificationService;
+
+    @GetMapping()
+    public ResponseEntity<List<NotificationResponse>> findNotification() {
+        return ResponseEntity.ok(findNotificationService.execute());
+    }
 }
