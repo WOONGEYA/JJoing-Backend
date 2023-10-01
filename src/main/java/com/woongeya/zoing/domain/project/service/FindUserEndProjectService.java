fc2916e73c4205f7aff0efd9ac6ed631e4bc1b03
Woongbin06 <kob05314@gmail.com>
2023-10-01 23:35:36 +0900

    UPDATE :: 다른 유저 프로젝트 조회 임시 수정

--- src/main/java/com/woongeya/zoing/domain/project/service/FindUserEndProjectService.java
+++ src/main/java/com/woongeya/zoing/domain/project/service/FindUserEndProjectService.java
@@ -1,9 +1,13 @@
 package com.woongeya.zoing.domain.project.service;
 
 import com.woongeya.zoing.domain.like.domain.repository.LikeRepository;
+import com.woongeya.zoing.domain.project.domain.Member;
+import com.woongeya.zoing.domain.project.domain.repository.MemberRepository;
 import com.woongeya.zoing.domain.project.domain.repository.ProjectRepository;
 import com.woongeya.zoing.domain.project.domain.type.ProjectState;
 import com.woongeya.zoing.domain.project.presetation.dto.response.ProjectResponseDto;
+import com.woongeya.zoing.domain.user.UserFacade;
+import com.woongeya.zoing.domain.user.domain.User;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -15,12 +19,16 @@ import java.util.stream.Collectors;
 @RequiredArgsConstructor
 public class FindUserEndProjectService {
 
+    private final MemberRepository memberRepository;
     private final ProjectRepository projectRepository;
     private final LikeRepository likeRepository;
 
     @Transactional(readOnly = true)
     public List<ProjectResponseDto> execute(Long id) {
-        return projectRepository.findProject(id).stream()
+        List<Member> members = memberRepository.findByUserId(id);
+
+        return members.stream()
+                .map(Member::getProject)
                 .filter(project -> project.getState().equals(ProjectState.FOUND))
                 .map(project -> {
                     Integer likeCount = likeRepository.countByProjectId(project.getId());
