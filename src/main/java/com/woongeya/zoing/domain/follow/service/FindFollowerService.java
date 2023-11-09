67d55e752f1d80178cadaa58ea8e01b85a23929a
Woongbin06 <kob05314@gmail.com>
2023-11-09 15:25:16 +0900

    FEAT :: 팔로우 확인 여부

--- src/main/java/com/woongeya/zoing/domain/follow/service/FindFollowerService.java
+++ src/main/java/com/woongeya/zoing/domain/follow/service/FindFollowerService.java
@@ -5,6 +5,7 @@ import com.woongeya.zoing.domain.follow.domain.repository.FollowRepository;
 import com.woongeya.zoing.domain.follow.presetation.dto.reponse.FollowResponseDto;
 import com.woongeya.zoing.domain.user.UserFacade;
 import com.woongeya.zoing.domain.user.domain.User;
+import com.woongeya.zoing.global.util.SecurityUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -20,14 +21,20 @@ public class FindFollowerService {
     private final FollowRepository followRepository;
 
     @Transactional
-
     public List<FollowResponseDto> execute(Long id) {
+        User currentUser = SecurityUtil.getCurrentUserOrNull();
         User user = userFacade.getUserById(id);
         List<Follow> follows = followRepository.findByFromUserId(user.getId());
 
         return follows.stream()
-                .map(follow -> userFacade.getUserById(follow.getToUserId()))
-                .map(FollowResponseDto::of)
+                .map(follow -> {
+                    User toUser = userFacade.getUserById(follow.getToUserId());
+                    return FollowResponseDto.of(toUser, currentUser != null && checkFollow(toUser, currentUser));
+                })
                 .collect(Collectors.toList());
+    }
+
+    private boolean checkFollow(User toUser, User currentUser) {
+        return followRepository.existsByToUserIdAndFromUserId(toUser.getId(), currentUser.getId());
     }
 }
--- src/main/java/com/woongeya/zoing/domain/follow/service/FindFollowingService.java
+++ src/main/java/com/woongeya/zoing/domain/follow/service/FindFollowingService.java
@@ -5,6 +5,7 @@ import com.woongeya.zoing.domain.follow.domain.repository.FollowRepository;
 import com.woongeya.zoing.domain.follow.presetation.dto.reponse.FollowResponseDto;
 import com.woongeya.zoing.domain.user.UserFacade;
 import com.woongeya.zoing.domain.user.domain.User;
+import com.woongeya.zoing.global.util.SecurityUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -22,12 +23,19 @@ public class FindFollowingService {
     @Transactional
 
     public List<FollowResponseDto> execute(Long id) {
+        User currentUser = SecurityUtil.getCurrentUserOrNull();
         User user = userFacade.getUserById(id);
         List<Follow> follows = followRepository.findByToUserId(user.getId());
 
         return follows.stream()
-                .map(follow -> userFacade.getUserById(follow.getToUserId()))
-                .map(FollowResponseDto::of)
+                .map(follow -> {
+                    User fromUser = userFacade.getUserById(follow.getFromUserId());
+                    return FollowResponseDto.of(fromUser, currentUser != null && checkFollow(fromUser, currentUser));
+                })
                 .collect(Collectors.toList());
+    }
+
+    private boolean checkFollow(User toUser, User currentUser) {
+        return followRepository.existsByToUserIdAndFromUserId(toUser.getId(), currentUser.getId());
     }
 }
