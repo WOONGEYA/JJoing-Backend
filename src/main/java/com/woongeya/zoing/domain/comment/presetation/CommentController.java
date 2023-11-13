8a7ad6b26a2e675dbb0d75a74383bd9dff653b13
Woongbin06 <kob05314@gmail.com>
2023-11-13 15:52:53 +0900

    FEAT :: 댓글 삭제

--- src/main/java/com/woongeya/zoing/domain/comment/presetation/CommentController.java
+++ src/main/java/com/woongeya/zoing/domain/comment/presetation/CommentController.java
@@ -3,6 +3,7 @@ package com.woongeya.zoing.domain.comment.presetation;
 import com.woongeya.zoing.domain.comment.presetation.dto.request.CreateCommentRequest;
 import com.woongeya.zoing.domain.comment.presetation.dto.response.CommentResponse;
 import com.woongeya.zoing.domain.comment.service.command.CreateCommentService;
+import com.woongeya.zoing.domain.comment.service.command.DeleteCommentService;
 import com.woongeya.zoing.domain.comment.service.command.UpdateCommentService;
 import com.woongeya.zoing.domain.comment.service.query.QueryCommentListService;
 import io.swagger.v3.oas.annotations.Operation;
@@ -20,6 +21,7 @@ public class CommentController {
     private final CreateCommentService createCommentService;
     private final QueryCommentListService queryCommentListService;
     private final UpdateCommentService updateCommentService;
+    private final DeleteCommentService deleteCommentService;
 
     @PostMapping("/{id}")
     @Operation(summary = "댓글 생성")
@@ -38,6 +40,13 @@ public class CommentController {
     @Operation(summary = "댓글 수정")
     public ResponseEntity<Void> update(@PathVariable Long id, @RequestBody CreateCommentRequest request) {
         updateCommentService.execute(id, request);
+        return ResponseEntity.noContent().build();
+    }
+
+    @DeleteMapping("/{id}")
+    @Operation(summary = "댓글 삭제")
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+        deleteCommentService.execute(id);
         return ResponseEntity.noContent().build();
     }
 }
