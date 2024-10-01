ad4088999f477a43ccf5bca57ffb7edb4b18c7fe
Woongbin06 <kob05314@gmail.com>
2024-10-01 23:35:32 +0900

    REFACTOR(project) :: notificationDto record로 변경

--- src/main/java/com/woongeya/zoing/domain/post/presetation/dto/response/PostResponseList.java
+++ src/main/java/com/woongeya/zoing/domain/post/presetation/dto/response/PostResponseList.java
@@ -5,7 +5,7 @@ import java.util.List;
 public record PostResponseList(
     List<PostResponse> postResponses
 ) {
-    public static PostResponseList of(List<PostResponse> postResponses) {
+    public static PostResponseList from(List<PostResponse> postResponses) {
         return new PostResponseList(postResponses);
     }
 }
