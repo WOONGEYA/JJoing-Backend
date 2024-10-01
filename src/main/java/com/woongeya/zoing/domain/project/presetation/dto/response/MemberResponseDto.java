9011d1091381a10fc97255f32dbda3691636f685
Woongbin06 <kob05314@gmail.com>
2024-10-01 21:18:06 +0900

    REFACTOR(project) :: member Dto들 record로 변경

--- src/main/java/com/woongeya/zoing/domain/project/presetation/dto/response/MemberResponseDto.java
+++ src/main/java/com/woongeya/zoing/domain/project/presetation/dto/response/MemberResponseDto.java
@@ -1,23 +1,13 @@
 package com.woongeya.zoing.domain.project.presetation.dto.response;
 
-import com.woongeya.zoing.domain.project.domain.Member;
 import com.woongeya.zoing.domain.user.domain.User;
-import lombok.Builder;
-import lombok.Getter;
 
-@Getter
-@Builder
-public class MemberResponseDto {
-
-    private Long userId;
-    private String name;
-    private String imgUrl;
-
-    public static MemberResponseDto of(User user) {
-        return MemberResponseDto.builder()
-                .userId(user.getId())
-                .name(user.getName())
-                .imgUrl(user.getImgUrl())
-                .build();
+public record MemberResponseDto (
+    Long userId,
+    String name,
+    String imgUrl
+){
+    public static MemberResponseDto from(User user) {
+        return new MemberResponseDto(user.getId(), user.getName(), user.getImgUrl());
     }
 }
