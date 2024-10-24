47b870a12e4ff2f480c20eaf6f9fddf60a66571d
Woongbin06 <kob05314@gmail.com>
2024-10-25 00:56:01 +0900

    TEST(project) :: ProjectTest 작성

---
+++ src/test/java/com/woongeya/zoing/project/domain/ProjectTest.java
@@ -0,0 +1,26 @@
+package com.woongeya.zoing.project.domain;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import com.woongeya.zoing.domain.project.domain.Project;
+import com.woongeya.zoing.domain.project.domain.type.ProjectState;
+
+public class ProjectTest {
+
+	@Test
+	void 프로젝트_인원수가_다_찰_경우_모집이_마감된다() {
+		// given
+		Project 쪼잉 = Project.builder()
+			.name("쪼잉")
+			.requiredPeople(4)
+			.currentPeople(4)
+			.build();
+
+		// when
+		쪼잉.isFull();
+
+		// then
+		Assertions.assertThat(쪼잉.getState()).isEqualTo(ProjectState.FOUND);
+	}
+}
